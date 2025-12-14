6be55819c034334ff651c04ac0fdc1d51f16ac20
puddingtonnn <120944647+puddingtonnn@users.noreply.github.com>
2025-12-15 00:47:02 +0900

    fix: deleted custom port

--- internal/transport/http/handler/auth_page.go
+++ internal/transport/http/handler/auth_page.go
@@ -12,7 +12,7 @@ func (h *AuthHandler) ServeTelegramLoginPage(w http.ResponseWriter, r *http.Requ
             <script async src="https://telegram.org/js/telegram-widget.js?22" 
                 data-telegram-login="meetuperbot" 
                 data-size="large" 
-                data-auth-url="https://meetuper.site:8443/auth/telegram/callback" 
+                data-auth-url="https://meetuper.site/auth/telegram/callback" 
                 data-request-access="write"></script>
         </div>
     </body>
